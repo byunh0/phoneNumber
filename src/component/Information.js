c1bcf00723846a71bb4520af1888f26f1c887d47
Byunh0 <bhy1999313@naver.com>
2024-08-26 21:29:51 +0900

    trying2

--- src/component/Information.js
+++ src/component/Information.js
@@ -3,11 +3,20 @@ import Numberbox from './Numberbox'
 import Searchbox from './Searchbox'
 import'../App.css'
 import{useSelector} from 'react-redux'
+import  { useState, useEffect } from 'react';
 const Information = () => {
-  
+  const [showAllResults, setShowAllResults] = useState(true);
   const contactList =useSelector(state=>state.contactList);
   const searchList = useSelector(state => state.searchList);
 
+
+  const handleSearch = (resultsExist) => {
+    setShowAllResults(!resultsExist); 
+  };
+
+  useEffect(() => {
+    setShowAllResults(searchList.length === 0);
+  }, [searchList]);
   return (
     <div className="namesearch-style">
     <Searchbox />
@@ -21,14 +30,16 @@ const Information = () => {
         <p>찾지 못했습니다</p>
       )}
     </div>
-    <div>
-      <h5>전체결과:</h5>
-      {contactList.map((item, index) => (
-        <Numberbox key={index} item={item} />
-      ))}
+    {showAllResults && (
+        <div>
+          <h5>전체결과:</h5>
+          {contactList.map((item, index) => (
+            <Numberbox key={index} item={item} />
+          ))}
+        </div>
+      )}
     </div>
-  </div>
-);
+  );
 };
 
 export default Information
